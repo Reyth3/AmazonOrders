dbb5641a596eb40bf4da2f56d2be3f8996730bf7
Reyth3 <mrreynevan2@gmail.com>
2016-12-04 12:46:54 +0100

    Added Models

---
+++ AmazonOrders/Models/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmazonOrders.Models
+{
+    class Item
+    {
+        public string Name { get; set; }
+        public string SoldBy { get; set; }
+        public string Price { get; set; }
+    }
+}
---
+++ AmazonOrders/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmazonOrders.Models
+{
+    class Order
+    {
+        public string Placed { get; set; }
+        public string Total { get; set; }
+    }
+}
